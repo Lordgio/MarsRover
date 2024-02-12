92189c77fcfa90eeeb47fa7fc87e7ba76babade4
Jorge Roldan <jroldanreal@gmail.com>
2024-02-12 19:49:13 +0100

    Simplified loading layout

--- ui/src/main/java/dev/jorgeroldan/marsrover/ui/components/FullScreenLoader.kt
+++ ui/src/main/java/dev/jorgeroldan/marsrover/ui/components/FullScreenLoader.kt
@@ -1,14 +1,9 @@
 package dev.jorgeroldan.marsrover.ui.components
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -21,20 +16,7 @@ fun FullScreenLoader(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .testTag("LoadingScreen"),
         contentAlignment = Alignment.Center) {
-        val infiniteTransition = rememberInfiniteTransition(label = "LoaderAnimation")
 
-        val rotation by infiniteTransition.animateFloat(
-            initialValue = 0F,
-            targetValue = 360F,
-            animationSpec = infiniteRepeatable(
-                animation = tween(durationMillis = 3000, easing = LinearEasing)
-            ),
-            label = "RotationAnimation"
-        )
-
-        /*Image(
-            painter = painterResource(id = "R.drawable."),
-            contentDescription = "Loading",
-            modifier = Modifier.rotate(rotation))*/
+        CircularProgressIndicator()
     }
 }
