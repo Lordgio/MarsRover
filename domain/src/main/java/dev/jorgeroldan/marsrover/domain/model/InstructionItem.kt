08ff85aae25c96820f7c918b1e9d59c024888d39
Jorge Roldan <jroldanreal@gmail.com>
2024-02-12 19:50:53 +0100

    Make several classes serializable and created toString for RoverMovement

--- domain/src/main/java/dev/jorgeroldan/marsrover/domain/model/InstructionItem.kt
+++ domain/src/main/java/dev/jorgeroldan/marsrover/domain/model/InstructionItem.kt
@@ -1,17 +1,19 @@
 package dev.jorgeroldan.marsrover.domain.model
 
+import java.io.Serializable
+
 data class InstructionItem(
     val topRightCorner: Coordinates,
     val roverPosition: Coordinates,
     val roverDirection: RoverDirection,
     val encodedMovements: String,
     val movements: List<RoverMovement>,
-)
+) : Serializable
 
 data class Coordinates(
     val x: Int,
     val y: Int,
-)
+) : Serializable
 
 enum class RoverDirection {
     NORTH, SOUTH, EAST, WEST;
@@ -26,4 +28,14 @@ enum class RoverDirection {
     }
 }
 
-enum class RoverMovement { SPIN_LEFT, SPIN_RIGHT, MOVE }
+enum class RoverMovement {
+    SPIN_LEFT, SPIN_RIGHT, MOVE;
+
+    override fun toString(): String {
+        return when (this) {
+            SPIN_LEFT -> "L"
+            SPIN_RIGHT -> "R"
+            MOVE -> "M"
+        }
+    }
+}
--- domain/src/main/java/dev/jorgeroldan/marsrover/domain/model/InstructionResolution.kt
+++ domain/src/main/java/dev/jorgeroldan/marsrover/domain/model/InstructionResolution.kt
@@ -1,12 +1,14 @@
 package dev.jorgeroldan.marsrover.domain.model
 
+import java.io.Serializable
+
 data class InstructionResolution(
     val initial: InstructionItem,
     val finalPosition: Coordinates,
     val finalOrientation: RoverDirection,
     val finalResolution: String,
     val steps: List<MovementStep>
-)
+) : Serializable
 
 data class MovementStep(
     val initialCoordinates: Coordinates,
@@ -15,10 +17,10 @@ data class MovementStep(
     val finalOrientation: RoverDirection,
     val movement: RoverMovement,
     val movementDescription: String,
-)
+) : Serializable
 
 data class FriendlyUserText(
     val startText: String,
     val moveText: String,
     val rotateText: String,
-)
+) : Serializable
