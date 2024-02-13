db9a3ca6c3f29bf394dbbaf9133c3c8201495b40
Jorge Roldan <jroldanreal@gmail.com>
2024-02-13 17:48:05 +0100

    Added more options to retrofit config

--- data/src/main/java/dev/jorgeroldan/marsrover/data/di/DataModule.kt
+++ data/src/main/java/dev/jorgeroldan/marsrover/data/di/DataModule.kt
@@ -5,13 +5,15 @@ import dev.jorgeroldan.marsrover.data.remote.RetrofitConfig
 import dev.jorgeroldan.marsrover.data.repository.MarsRoverRepositoryImpl
 import dev.jorgeroldan.marsrover.domain.repository.MarsRoverRepository
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 object DataModule {
     val module = module {
         single<Json> { Json }
-        single<Retrofit> { RetrofitConfig.buildRetrofit(get()) }
+        single<OkHttpClient> { RetrofitConfig.buildHttpClient() }
+        single<Retrofit> { RetrofitConfig.buildRetrofit(get(), get()) }
         single<MarsRoverApi> { get<Retrofit>().create(MarsRoverApi::class.java) }
         factory<MarsRoverRepository> { MarsRoverRepositoryImpl(get()) }
     }
--- data/src/main/java/dev/jorgeroldan/marsrover/data/remote/RetrofitConfig.kt
+++ data/src/main/java/dev/jorgeroldan/marsrover/data/remote/RetrofitConfig.kt
@@ -4,18 +4,28 @@ import arrow.retrofit.adapter.either.EitherCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 
 object RetrofitConfig {
 
-    fun buildRetrofit(json: Json): Retrofit {
+    fun buildRetrofit(okHttpClient: OkHttpClient, json: Json): Retrofit {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl("https://lordgio.github.io/MarsRover/mockdata/")
             .addConverterFactory(json.asConverterFactory(
                 contentType = MediaType.get("application/json")
             ))
             .addCallAdapterFactory(EitherCallAdapterFactory())
+            .build()
+    }
+
+    fun buildHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
             .build()
     }
 }
